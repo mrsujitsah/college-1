dcc444118a9512bc0ff5ec780d6abfd2337e4758
rushikesh <rushighonse73730@gmail.com>
2020-09-08 19:45:27 +0530

    string operation for asingment one

--- fds/stringopration.py
+++ fds/stringopration.py
@@ -1,1 +1,15 @@
 #string opration
+str1=input("enter the string ")
+print("the string is ",str1)
+
+list1=str1.split(" ")
+print("list is",list1 )
+
+maxLen = -1
+for word in list1:
+    if len(word) >= maxLen:
+        maxLen = len(word)
+        result = word
+# printing result
+print("Maximum length string is : " + result)
+
